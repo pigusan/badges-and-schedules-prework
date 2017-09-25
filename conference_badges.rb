d07c5658de316ddbefea072283f8dba224375ff8
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 18:29:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,1 @@
 # Write your code here.
-def badge_maker(name)
-  `Hello, my name is #{name}.`
-end
-
-def batch_badge_creator(attendees)
-  message = []
-  attendees.each do |attendee|
-    message.push(badge_maker(attendee))
-  end
-  message
-end
-
-def assign_rooms
