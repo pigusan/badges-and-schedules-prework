a1c73bf59381f4b034acdf77fedced6c3d97378f
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 18:45:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,8 @@ def assign_rooms(attendees)
   badge_messages_and_romm_assignments = []
   attendees.each_with_index do |attendee, index|
     badge_messages_and_romm_assignments.push(`Welcome #{attendee}! You'll be assigned to room #{index + 1}!`)
+  end
+  badge_messages_and_romm_assignments
 end
 
 def printer(attendees)
