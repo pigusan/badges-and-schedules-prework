c433161a2b2dba62bd277eb5f187779351ee321f
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 18:57:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator.each do |attendee|
+  batch_badge_creator(attendees).each do |attendee|
     puts attendee
   end
   assign_rooms(attendees).each do |attendee|
